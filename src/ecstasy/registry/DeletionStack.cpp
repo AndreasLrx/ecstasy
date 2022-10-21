5a68ee86c6cd383573dbc42a922e4700a439f977
Andreas Leroux <andreas.leroux@epitech.eu>
2022-10-21 17:02:29 +0200

    Introduce scope-safe entity deletion with DeletionStack
    Linked: #16

---
+++ src/ecstasy/registry/DeletionStack.cpp
@@ -0,0 +1,35 @@
+///
+/// @file DeletionStack.cpp
+/// @author Andréas Leroux (andreas.leroux@epitech.eu)
+/// @brief
+/// @version 1.0.0
+/// @date 2022-10-21
+///
+/// @copyright Copyright (c) ECSTASY 2022
+///
+///
+
+#include "DeletionStack.hpp"
+#include "ecstasy/registry/Registry.hpp"
+
+namespace ecstasy
+{
+    DeletionStack::DeletionStack(Registry &registry) : _registry(registry)
+    {
+    }
+
+    DeletionStack::~DeletionStack()
+    {
+        _registry.eraseEntities(_killed);
+    }
+
+    void DeletionStack::push(Entity entity)
+    {
+        _killed.push_back(entity);
+    }
+
+    size_t DeletionStack::size() const
+    {
+        return _killed.size();
+    }
+} // namespace ecstasy
