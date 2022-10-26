652bd00a00433d4b6f0efa09ae3aeb71dbb921fa
Andreas Leroux <andreas.leroux@epitech.eu>
2022-10-26 11:35:43 +0200

    Introduce Registry Maybe modifier, not implemented yet
    Linked: #27

---
+++ src/ecstasy/registry/modifiers/Maybe.hpp
@@ -0,0 +1,34 @@
+///
+/// @file Maybe.hpp
+/// @author Andréas Leroux (andreas.leroux@epitech.eu)
+/// @brief
+/// @version 1.0.0
+/// @date 2022-10-25
+///
+/// @copyright Copyright (c) ECSTASY 2022
+///
+///
+
+#ifndef ECSTASY_REGISTRY_MODIFIERS_MAYBE_HPP_
+#define ECSTASY_REGISTRY_MODIFIERS_MAYBE_HPP_
+
+#include "ecstasy/query/modifiers/Maybe.hpp"
+
+namespace ecstasy
+{
+    ///
+    /// @brief Placeholder for @ref ecstasy::query::modifier::Maybe.
+    ///
+    /// @tparam C Component type.
+    ///
+    /// @author Andréas Leroux (andreas.leroux@epitech.eu)
+    /// @since 1.0.0 (2022-10-24)
+    ///
+    template <typename C>
+    struct Maybe {
+        using QueryData = C;
+        using Modifier = query::modifier::Maybe<getStorageType<C>>;
+    };
+} // namespace ecstasy
+
+#endif /* !ECSTASY_REGISTRY_MODIFIERS_MAYBE_HPP_ */
