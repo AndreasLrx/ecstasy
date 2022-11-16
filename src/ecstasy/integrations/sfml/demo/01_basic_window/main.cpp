76abc7ce26d65438866cea5beefc845a00893ea2
Andreas Leroux <andreas.leroux@epitech.eu>
2022-11-16 14:01:22 +0100

    Introduce ObjectWrapper registry Resource
    Linked: #59

--- src/ecstasy/integrations/sfml/demo/01_basic_window/main.cpp
+++ src/ecstasy/integrations/sfml/demo/01_basic_window/main.cpp
@@ -12,23 +12,27 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "ecstasy/integrations/sfml/resources/RenderWindow.hpp"
 #include "ecstasy/registry/Registry.hpp"
+
+namespace esf = ecstasy::integration::sfml;
 
 int main(int argc, char **argv)
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "ECSTASY SFML integration");
     ecstasy::Registry registry;
+    esf::RenderWindow &window =
+        registry.addResource<esf::RenderWindow>(sf::VideoMode(1280, 720), "ECSTASY SFML integration");
     (void)argv;
 
     sf::Event e;
-    while (window.isOpen()) {
-        window.clear(sf::Color::White);
-        while (window.pollEvent(e))
+    while (window.get().isOpen()) {
+        window.get().clear(sf::Color::White);
+        while (window.get().pollEvent(e))
             if (e.type == sf::Event::Closed) {
-                window.close();
+                window.get().close();
                 break;
             }
-        window.display();
+        window.get().display();
     }
     return argc;
 }
