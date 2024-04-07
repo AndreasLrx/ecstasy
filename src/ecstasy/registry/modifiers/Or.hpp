e9e100744d789fb80eb45907a407e22c1f17ede1
Andreas Leroux <andreas.leroux@epitech.eu>
2024-04-07 20:24:51 +0200

    Introduce queryEx, whereEx and [modifier]Ex extended version to explicitly specify AutoLock
    Linked: #129

--- src/ecstasy/registry/modifiers/Or.hpp
+++ src/ecstasy/registry/modifiers/Or.hpp
@@ -32,6 +32,23 @@ namespace ecstasy
         using Modifier = query::modifier::OrImpl<thread::AUTO_LOCK_DEFAULT, queryable_type_t<C1>, queryable_type_t<C2>,
             queryable_type_t<Cs>...>;
     };
+
+    ///
+    /// @brief Placeholder for @ref ecstasy::query::modifier::Or. This extended version allows to specify the AutoLock.
+    ///
+    /// @tparam AutoLock Whether the queryables should be locked automatically.
+    /// @tparam C1 left operand type.
+    /// @tparam C2 right operand type.
+    /// @tparam Cs additional operand types.
+    ///
+    /// @author Andréas Leroux (andreas.leroux@epitech.eu)
+    /// @since 1.0.0 (2022-10-24)
+    ///
+    template <bool AutoLock, typename C1, typename C2, typename... Cs>
+    struct OrEx {
+        using Modifier =
+            query::modifier::OrImpl<AutoLock, queryable_type_t<C1>, queryable_type_t<C2>, queryable_type_t<Cs>...>;
+    };
 } // namespace ecstasy
 
 #endif /* !ECSTASY_REGISTRY_MODIFIERS_OR_HPP_ */
